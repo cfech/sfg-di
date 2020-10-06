a684547e5a9f1cc0b0da4fb3e8ad44b5bc67b9ee
Connor <cfech44@gmail.com>
2020-10-06 16:55:29 -0400

    adding deafualt profiles

--- src/main/java/com/example/sfgdi/services/I8TnSpanishGreetingService.java
+++ src/main/java/com/example/sfgdi/services/I8TnSpanishGreetingService.java
@@ -3,7 +3,7 @@ package com.example.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("es")
+@Profile({"es", "default"})
 @Service("I8TnService")
 public class I8TnSpanishGreetingService implements  GreetingService{
     @Override
